04637c156dc1069d3147ad253434ae226cf3d0c0
Eimihar <newrehmi@gmail.com>
2018-07-17 16:42:25 +0800

    gitignore explude samples meta, and repaint on exit

--- src/Editors/NodeEditor.cs
+++ src/Editors/NodeEditor.cs
@@ -345,6 +345,7 @@ namespace DialogueSmith.Editors
                             return;
 
                         Uninitialize();
+                        Repaint();
                     });
                     
                     menu.AddSeparator("");
